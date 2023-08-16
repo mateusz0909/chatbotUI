6cfb61ddfe2b9f798fff23e918c0d4f644f6aeb9
Mateusz Byrtus <mateusz.byrtus@gmail.com>
2023-08-16 14:01:51 +0200

    css adjusted

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 import streamlit as st
 import time
 
-class BobChat:
+class AIChat:
     USERNAME = 'user'
-    PASSWORD = 'password'
+    PASSWORD = '1234'
     CATEGORIES = ['Baby Care', 'Supplements', 'Vaccines', 'Oral Care', 'Pet Care']
 
     def __init__(self):
@@ -62,23 +62,22 @@ class BobChat:
         st.title("1. Select Category")
         category = st.selectbox('Select from the list:', self.CATEGORIES)
 
-        st.title('2. Ask Bob')
+        st.title('2. Ask AI')
 
         chat_container = st.container()
 
         with chat_container:
             with st.chat_message("assistant"):
-                st.write(f"Hi, I'm Bob. How can I assist you with {category}?")
+                st.write(f"Hi, I'm AI Assistant. How can I assist you with {category}?")
 
             options = [
-                '❓ Ask Bob a Question',
-                '🗒️ Get Suggested Questions',
-                '🚀 Explore Insights and Analysis'
+                '❓ Ask a Question',
+                '🚀 Explore Insights'
             ]
 
             option = st.radio('Choose an option:', options)
 
-            if option == '❓ Ask Bob a Question':
+            if option == '❓ Ask a Question':
                 prompt = st.chat_input('Type your question here...')
                 if prompt:
                     with chat_container:
@@ -87,36 +86,36 @@ class BobChat:
                         with st.chat_message("assistant"):
                             st.write('Answer to your question about ' + category + ': ...')
 
+            #
+            # elif option == '🗒️ Get Suggested Questions':
+            #     topic_question = st.selectbox('Select Topic:', [
+            #         'Value perception',
+            #         'Quality',
+            #         'Smell',
+            #         'Type',
+            #         'Sustainability'
+            #     ])
+            #
+            #     selected_question = st.radio('Choose a question:', [
+            #         'What are the most common words or phrases used by customers to describe the value perception of the product/service?',
+            #         'How do customers perceive the pricing of the product/service? Is it considered affordable or expensive?',
+            #         'Are there any specific promotions or discounts that customers mention in their reviews? How do these affect their perception of value?',
+            #         'Do customers mention any specific features or benefits that they find particularly valuable in relation to the price they paid?',
+            #         'Are there any negative comments about the value perception of the product/service? If so, what are the main reasons for this perception?'
+            #     ])
+            #
+            #     submit_question = st.button('Submit Question')
+            #
+            #     if submit_question and selected_question:
+            #         with chat_container:
+            #             with st.chat_message("user"):
+            #                 st.write(selected_question)
+            #             with st.chat_message("assistant"):
+            #                 with st.spinner(text='Working on it...'):
+            #                     time.sleep(3)
+            #                     st.write('Answer to your question: ...')
 
-            elif option == '🗒️ Get Suggested Questions':
-                topic_question = st.selectbox('Select Topic:', [
-                    'Value perception',
-                    'Quality',
-                    'Smell',
-                    'Type',
-                    'Sustainability'
-                ])
-
-                selected_question = st.radio('Choose a question:', [
-                    'What are the most common words or phrases used by customers to describe the value perception of the product/service?',
-                    'How do customers perceive the pricing of the product/service? Is it considered affordable or expensive?',
-                    'Are there any specific promotions or discounts that customers mention in their reviews? How do these affect their perception of value?',
-                    'Do customers mention any specific features or benefits that they find particularly valuable in relation to the price they paid?',
-                    'Are there any negative comments about the value perception of the product/service? If so, what are the main reasons for this perception?'
-                ])
-
-                submit_question = st.button('Submit Question')
-
-                if submit_question and selected_question:
-                    with chat_container:
-                        with st.chat_message("user"):
-                            st.write(selected_question)
-                        with st.chat_message("assistant"):
-                            with st.spinner(text='Working on it...'):
-                                time.sleep(3)
-                                st.write('Answer to your question: ...')
-
-            elif option == '🚀 Explore Insights and Analysis':
+            elif option == '🚀 Explore Insights':
                 analysis_type = st.selectbox('Select Analysis Type:', [
                     'R&R: Topic pros and cons',
                     'R&R: Topic competitor compare',
@@ -163,4 +162,4 @@ class BobChat:
                                 st.write(response)
 
 if __name__ == "__main__":
-    app = BobChat()
+    app = AIChat()
